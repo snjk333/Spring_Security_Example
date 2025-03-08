5b0a565ba0ce18840bee377de2da4b237edee016
= <=>
2025-03-08 09:57:08 +0100

    add HikariCP DataSource bean

--- src/main/java/com/kulbit/spring/security/configuration/MyConfig.java
+++ src/main/java/com/kulbit/spring/security/configuration/MyConfig.java
@@ -1,5 +1,7 @@
 package com.kulbit.spring.security.configuration;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,8 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "com.kulbit.spring.security")
@@ -22,6 +26,18 @@ public class MyConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName("com.mysql.jdbc.Driver");
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
+        config.setUsername("bestuser");
+        config.setPassword("bestuser");
+
+        return new HikariDataSource(config);
     }
 
 }
