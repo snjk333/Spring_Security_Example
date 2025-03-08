f132d0788e166d3facf803b092478b5052faccc6
= <=>
2025-03-08 10:10:52 +0100

    Configure Spring Security with HttpSecurity

---
+++ src/main/java/com/kulbit/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,41 @@
+package com.kulbit.spring.security.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig {
+
+    DataSource dataSource;
+
+    public MySecurityConfig(@Autowired DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/").hasAnyRole("EMPLOYEE", "MANAGER", "HR")
+                        .requestMatchers("/hr_info").hasRole("HR")
+                        .requestMatchers("/manager_info/**").hasRole("MANAGER")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
